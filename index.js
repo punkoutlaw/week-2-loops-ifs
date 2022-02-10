7a445a8196708519ed69514000e2c2b76d9e527b
David Feudale <dwfeudale@gmail.com>
2022-02-09 20:31:01 -0500

    first commit

--- index.js
+++ index.js
@@ -1,1 +1,25 @@
-console.log("Hello Bob");
+// var myAge = window.prompt("How old are you?")
+// console.log(myAge)
+
+// if statements only evaluate booleans
+// debugger
+
+// if(myAge > 6) {
+//     console.log("this was true")
+// } else {
+//     console.log("not greater than 6")
+// }
+//
+
+// if(myAge > 55) {
+//     console.log("50%")
+// } else if (myAge == 55) {
+//     console.log("25%")
+// } else {
+//     console.log("No dicount")
+// }
+
+var hello = !!'hello';
+if(hello) {
+    console.log('hello')
+}
